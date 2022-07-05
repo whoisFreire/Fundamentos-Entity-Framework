fd278bd90f55e43aa0f86184864db130a789e05d
Leonardo Freire <leonardofreire.russi@icloud.com>
2022-07-05 02:42:47 -0300

    first commit

---
+++ Migrations/20220705045956_GithubField.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    public partial class GithubField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Github",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "Post",
+                type: "SMALLDATETIME",
+                nullable: false,
+                defaultValue: new DateTime(2022, 7, 5, 4, 59, 56, 770, DateTimeKind.Utc).AddTicks(3435),
+                oldClrType: typeof(DateTime),
+                oldType: "SMALLDATETIME",
+                oldDefaultValue: new DateTime(2022, 7, 5, 4, 53, 14, 939, DateTimeKind.Utc).AddTicks(8657));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Github",
+                table: "User");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "Post",
+                type: "SMALLDATETIME",
+                nullable: false,
+                defaultValue: new DateTime(2022, 7, 5, 4, 53, 14, 939, DateTimeKind.Utc).AddTicks(8657),
+                oldClrType: typeof(DateTime),
+                oldType: "SMALLDATETIME",
+                oldDefaultValue: new DateTime(2022, 7, 5, 4, 59, 56, 770, DateTimeKind.Utc).AddTicks(3435));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
